b0d9c7d8fc22e5c0b4a10ab1c340c7c6cd0bbdbb
Jorge Ribeiro <joorgemelo@gmail.com>
2017-04-27 15:42:35 -0300

    First commit

---
+++ Synthetic OCT/Synthetic OCT/Synthetic_OCT.py
@@ -0,0 +1,16 @@
+﻿__author__ = 'Jorge Ribeiro'
+__email__ = 'joorgemelo@gmail.com'
+
+import scipy.io as sio
+import os
+import numpy as np
+from matplotlib import pyplot as plt
+
+os.chdir('data')
+matfile = sio.loadmat('Farsiu_Ophthalmology_2013_Control_Subject_1001.mat', squeeze_me=True, struct_as_record=False)
+print matfile
+image = np.array(matfile['images'])
+print image.shape
+plt.figure()
+plt.imshow(image[:,:,12,],'gray')
+plt.show()
