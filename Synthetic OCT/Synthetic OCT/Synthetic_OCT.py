0df0ee15548d652c675fe6a25cbb8a45741f4dfa
Jorge Ribeiro <joorgemelo@gmail.com>
2017-05-09 09:02:05 -0300

    A few updates

--- Synthetic OCT/Synthetic OCT/Synthetic_OCT.py
+++ Synthetic OCT/Synthetic OCT/Synthetic_OCT.py
@@ -4,12 +4,12 @@ __email__ = 'joorgemelo@gmail.com'
 import scipy.io as sio
 import os
 import numpy as np
-from matplotlib import pyplot as plt
+import matplotlib.pyplot as plt
 
-os.chdir('data')
-matfile = sio.loadmat('Farsiu_Ophthalmology_2013_Control_Subject_1001.mat', squeeze_me=True, struct_as_record=False)
-image = np.array(matfile['layerMaps'])
+matfile = sio.loadmat('data/Farsiu_Ophthalmology_2013_Control_Subject_1002.mat', squeeze_me=True, struct_as_record=False)
+print matfile
+image = np.array(matfile['images'])
 print image.shape
 plt.figure()
-plt.imshow(image[:,:,:,],'gray')
+plt.imshow(image[:,:,2], 'gray')
 plt.show()
