84607af23811e30a31efc90aea4be454392f6c41
Jorge Ribeiro <joorgemelo@gmail.com>
2017-05-18 11:38:42 -0300

    New stuff added

--- Synthetic OCT/Synthetic OCT/Synthetic_OCT.py
+++ Synthetic OCT/Synthetic OCT/Synthetic_OCT.py
@@ -6,9 +6,10 @@ import os
 import numpy as np
 import matplotlib.pyplot as plt
 
-matfile = sio.loadmat('data/Farsiu_Ophthalmology_2013_Control_Subject_1002.mat', squeeze_me=True, struct_as_record=False)
-print matfile
+matfile = sio.loadmat('data/Subject_10.mat', squeeze_me=True, struct_as_record=False)
+#print matfile
 image = np.array(matfile['images'])
+print image
 print image.shape
 plt.figure()
 plt.imshow(image[:,:,2], 'gray')
