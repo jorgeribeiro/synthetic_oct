ace8cb721bce9b33aaf39292fef432d583f6d7a0
Jorge Ribeiro <joorgemelo@gmail.com>
2017-06-06 10:06:29 -0300

    Revert "Update functions and adding new data["
    This reverts commit bed0feb8e29e8693096a9fc6958ae243f9553751.

--- Synthetic OCT/Synthetic OCT/Synthetic_OCT.py
+++ Synthetic OCT/Synthetic OCT/Synthetic_OCT.py
@@ -2,14 +2,18 @@
 __email__ = 'joorgemelo@gmail.com'
 
 import scipy.io as sio
+import scipy.ndimage as sp 
 import scipy.misc as sv
+import os
 import numpy as np
-from aniso1 import anisodiff1
-from aniso2 import anisodiff2
+import matplotlib.pyplot as plt
+from scipy.ndimage.filters import gaussian_filter as gaussian
 
-#Load mat image
-matfile = sio.loadmat('data/oct_4.mat', squeeze_me=True, struct_as_record=False)
+matfile = sio.loadmat('data/Farsiu_Ophthalmology_2013_Control_Subject_1001.mat', squeeze_me=True, struct_as_record=False)
 img = np.array(matfile['images'])
+#plt.figure()
+#plt.imshow(img[:,:,2], 'gray')
+#plt.show()
 
 #Convert rgb to grayscale
 def rgb2gray(rgb):
@@ -17,8 +21,40 @@ def rgb2gray(rgb):
 img = rgb2gray(img);
 
 sv.imsave('normal.jpg', img)
-img1 = anisodiff1(img, niter=50, kappa=20, option=1)
-img2 = anisodiff2(img, niter=70, kappa=20, gamma=0.1, step=(1.,1.), option=1, ploton=False)
 
-sv.imsave('anisotropic1.jpg', img1)
-sv.imsave('anisotropic2.jpg', img2)
+def forwardDifferenceGradient(img):  
+	diffY = np.zeros_like(img)
+	diffX = np.zeros_like(img)
+	diffY[:-1, :] = np.diff(img, axis = 0)
+	diffX[:, :-1] = np.diff(img, axis = 1)
+	return diffY, diffX
+
+def sigmoidFormula(gradientY, gradientX, k):
+	cGradientY = np.exp(-(gradientY/k) **2.)
+	cGradientX = np.exp(-(gradientX/k) **2.)
+	YVal = cGradientY * gradientY
+	XVal = cGradientX * gradientX
+	return YVal, XVal
+
+def tanhFormula(gradientY, gradientX, k):
+	cGradientY = 1./(1. + ((gradientY/k)**2))
+	cGradientX = 1./(1. + ((gradientX/k)**2))
+	YVal = cGradientY * gradientY
+	XVal = cGradientX * gradientX
+	return YVal, XVal
+
+img = img.astype("float32")
+#gauss = gaussian(img, 11)
+shiftedY = np.zeros_like(img)
+shiftedX = np.zeros_like(img)
+for i in range(10):
+	dY, dX = forwardDifferenceGradient(img)
+	cY, cX = tanhFormula(dY, dX, 20)
+	shiftedY[:] = cY
+	shiftedX[:] = cX
+	shiftedY[1:,:] -= cY[:-1,:]
+	shiftedX[:,1:] -= cX[:,:-1]
+	img += 0.25*(shiftedY+shiftedX)
+
+sv.imsave('anisotropic.jpg', img)
+#sv.imsave('gaussian.jpg', gauss)
